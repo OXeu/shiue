40d1c572fabd438ba44f26d1dc2795c6eebd3efb
Xeu <i@xeu.life>
2026-09-22 02:56:24 +0000

    feat: remove comment and friend submission services
    Disable submission endpoints and approval, notification, and publishing workflows.

--- server/runtime/node.js
+++
@@ -1,5 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-export async function readPages(file = new URL('../../public/comment-pages.json', import.meta.url)) {
-  return JSON.parse(await readFile(file, 'utf8'));
-}
